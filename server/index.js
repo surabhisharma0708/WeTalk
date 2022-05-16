2d6bff922b296fb88e03143918aefd7c8c29b782
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-16 13:47:50 +0530

    including the modules

---
+++ server/index.js
@@ -0,0 +1,5 @@
+//we will set-up our server here
+//including the modules which are required
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
