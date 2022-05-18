3e1034d1b38f3fed9bc23b9c20304aa1a646b5a5
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-18 23:30:53 +0530

    created the server

--- server/index.js
+++ server/index.js
@@ -2,4 +2,15 @@
 //including the modules which are required
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");  //used to interect with mongodb server
+
+const app = express();  //here express function is exported from above module
+require("dotenv").config(); 
+
+app.use(cors());
+app.use(express.json());
+
+//creating a server
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server started on Port ${process.env.PORT}`)
+})
