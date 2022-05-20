a7f50db0fdb7c8bd428285e861d65955fdbc97f5
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2022-05-20 23:09:26 +0530

    added the validation

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {Link} from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../assets/logo.svg"
+import Logo from "../assets/logo.svg";
+import {ToastContainer,toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import axios from "axios";
 
 function Register() {
-  const handleSubmit = (event)=>{
+
+  const toastOptions ={
+    position:'bottom-right',
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable:true,
+    theme:"dark",
+  }
+  const [values,setValues] =useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+  });
+
+  const handleSubmit =async (event)=>{
     event.preventDefault();
-    alert("form");
+    if(handleValidation()){
+      const { password, confirmPassword,username,email } = values;
+      const {data} = await axios.post();
+    }
   };
-  const handleChange= (event)=>{};
+
+  const handleValidation = () =>{
+    const {password,confirmPassword, username, email } = values;
+    if(password !== confirmPassword)
+    {
+      toast.error("password and confirm password should be same.",toastOptions);
+      return false;
+    } else if(username.length<3)
+    {
+      toast.error("Username should be greater than 3 characters", toastOptions);
+      return false;
+    }else if(password.length<8)
+    {
+      toast.error("Password should be greater or eeual than 8 characters", toastOptions);
+      return false;
+    }
+    else if(email ===""){
+      toast.error("email is required", toastOptions);
+      return false;
+    }
+    return true;  
+  };
+  const handleChange= (event)=>{
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
   return (
     <>
       <FormContainer>
@@ -46,6 +91,7 @@ function Register() {
           <span>Already have an account ? <Link to="/login">Login</Link></span>
         </form>
       </FormContainer>
+      <ToastContainer/>
     </>
   );
 
